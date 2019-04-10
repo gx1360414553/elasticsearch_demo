99120b15ee9461c179ffaf8f4c5c29146445cbbb
gx1360414553 <1360414553@qq.com>
2019-04-10 18:17:09 +0800

    1

--- src/test/java/com/gx/test/EsDemo.java
+++ src/test/java/com/gx/test/EsDemo.java
@@ -1,7 +1,11 @@
 package com.gx.test;
 
+import org.elasticsearch.action.bulk.BulkRequestBuilder;
+import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.get.MultiGetItemResponse;
+import org.elasticsearch.action.get.MultiGetResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.action.update.UpdateRequest;
@@ -11,6 +15,10 @@ import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.TransportAddress;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.reindex.BulkByScrollResponse;
+import org.elasticsearch.index.reindex.DeleteByQueryAction;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import org.junit.Test;
 
@@ -18,6 +26,8 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 
 public class EsDemo {
@@ -28,7 +38,7 @@ public class EsDemo {
         Settings settings = Settings.builder().put("cluster.name", "my-application").build();
 
         //创建访问es服务器的客户端
-        TransportClient client = new PreBuiltTransportClient(settings).addTransportAddress(new TransportAddress(InetAddress.getByName("192.168.26.130"), 9300));
+        TransportClient client = new PreBuiltTransportClient(settings).addTransportAddress(new TransportAddress(InetAddress.getByName("192.168.229.128"), 9300));
 //        PUT /lib/user/1
 //        {
 //            "first_name":  "gao",
@@ -83,7 +93,7 @@ public class EsDemo {
         Settings settings = Settings.builder().put("cluster.name", "my-application").build();
 
         //创建访问es服务器的客户端
-        TransportClient client = new PreBuiltTransportClient(settings).addTransportAddress(new TransportAddress(InetAddress.getByName("192.168.26.130"), 9300));
+        TransportClient client = new PreBuiltTransportClient(settings).addTransportAddress(new TransportAddress(InetAddress.getByName("192.168.229.128"), 9300));
 
         //添加文档
         XContentBuilder doc = XContentFactory.jsonBuilder().startObject()
@@ -119,7 +129,7 @@ public class EsDemo {
         Settings settings = Settings.builder().put("cluster.name", "my-application").build();
 
         //创建访问es服务器的客户端
-        TransportClient client = new PreBuiltTransportClient(settings).addTransportAddress(new TransportAddress(InetAddress.getByName("192.168.26.130"), 9300));
+        TransportClient client = new PreBuiltTransportClient(settings).addTransportAddress(new TransportAddress(InetAddress.getByName("192.168.229.128"), 9300));
         //更新文档
         UpdateRequest request = new UpdateRequest();
         request.index("index1").type("blog").id("10")
@@ -139,7 +149,7 @@ public class EsDemo {
         Settings settings = Settings.builder().put("cluster.name", "my-application").build();
 
         //创建访问es服务器的客户端
-        TransportClient client = new PreBuiltTransportClient(settings).addTransportAddress(new TransportAddress(InetAddress.getByName("192.168.26.130"), 9300));
+        TransportClient client = new PreBuiltTransportClient(settings).addTransportAddress(new TransportAddress(InetAddress.getByName("192.168.229.128"), 9300));
         //upsert修改没有就新增
         IndexRequest request1 = new IndexRequest("index1", "blog", "8")
                 .source(XContentFactory.jsonBuilder()
@@ -159,5 +169,110 @@ public class EsDemo {
         UpdateResponse response = client.update(request2).get();
         System.out.println(response.status());
         client.close();
+    }
+
+    //mget查询
+    @Test
+    public void test6() throws UnknownHostException {
+        //指定ES集群cluster.name: my-application
+        Settings settings = Settings.builder().put("cluster.name", "my-application").build();
+
+        //创建访问es服务器的客户端
+        TransportClient client = new PreBuiltTransportClient(settings).addTransportAddress(new TransportAddress(InetAddress.getByName("192.168.229.128"), 9300));
+
+        MultiGetResponse responses = client.prepareMultiGet()
+                .add("index1", "blog", "8", "10")
+                .add("lib3", "user", "1", "2", "3").get();
+
+        for (MultiGetItemResponse item : responses) {
+            GetResponse response = item.getResponse();
+            if (response != null && response.isExists()) {
+                System.out.println(response.getSourceAsString());
+            }
+
+        }
+
+    }
+
+    //bulk批量操作
+    @Test
+    public void test7() throws IOException {
+        //指定ES集群cluster.name: my-application
+        Settings settings = Settings.builder().put("cluster.name", "my-application").build();
+
+        //创建访问es服务器的客户端
+        TransportClient client = new PreBuiltTransportClient(settings).addTransportAddress(new TransportAddress(InetAddress.getByName("192.168.229.128"), 9300));
+
+        BulkRequestBuilder builder = client.prepareBulk();
+
+        //批量添加
+        builder.add(client.prepareIndex("lib2", "books", "8")
+                .setSource(
+                        XContentFactory.jsonBuilder()
+                                .startObject()
+                                .field("title", "python")
+                                .field("price", 999)
+                                .endObject()
+                )
+        );
+        builder.add(client.prepareIndex("lib2", "books", "9")
+                .setSource(
+                        XContentFactory.jsonBuilder()
+                                .startObject()
+                                .field("title", "VR")
+                                .field("price", 889)
+                                .endObject()
+                )
+        );
+        BulkResponse responses = builder.get();
+        System.out.println(responses.status());
+        if (responses.hasFailures()) {
+            System.out.println("失败了");
+        }
+
+    }
+
+    //查询删除
+    @Test
+    public void test8() throws IOException {
+        //指定ES集群cluster.name: my-application
+        Settings settings = Settings.builder().put("cluster.name", "my-application").build();
+
+        //创建访问es服务器的客户端
+        TransportClient client = new PreBuiltTransportClient(settings).addTransportAddress(new TransportAddress(InetAddress.getByName("192.168.229.128"), 9300));
+
+        BulkByScrollResponse response = DeleteByQueryAction.INSTANCE
+                .newRequestBuilder(client)
+                .filter(QueryBuilders.matchQuery("title", "工厂"))
+                .source("index1")
+                .get();
+
+        long counts = response.getDeleted();
+        System.out.println(counts);
+
+    }
+
+    //match_all
+    @Test
+    public void test9() throws IOException {
+        //指定ES集群cluster.name: my-application
+        Settings settings = Settings.builder().put("cluster.name", "my-application").build();
+
+        //创建访问es服务器的客户端
+        TransportClient client = new PreBuiltTransportClient(settings).addTransportAddress(new TransportAddress(InetAddress.getByName("192.168.229.128"), 9300));
+
+        /*client.prepareSearch("lib3")
+                .setQuery(qb)*/
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        executorService.execute(new Runnable() {
+            public void run() {
+                for (int i = 0; i < 10000; i++) {
+                    System.out.println(i);
+                }
+                System.out.println("end................");
+            }
+        });
+
     }
 }
